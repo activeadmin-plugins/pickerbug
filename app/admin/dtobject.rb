ba8739eece5eb0d4240908d6453af9ee2f529ec3
Grant Birchmeier <gbirchmeier@connamara.com>
2016-06-13 16:57:14 -0500

    add active_admin_datetimepicker.  The bug is replicated.

--- app/admin/dtobject.rb
+++ app/admin/dtobject.rb
@@ -2,4 +2,10 @@ ActiveAdmin.register Dtobject do
 
   permit_params :name, :footime
 
+  form do |f|
+    f.semantic_errors(*f.object.errors.keys)
+    f.input :name
+    f.input :footime, as: :date_time_picker
+  end
+
 end
